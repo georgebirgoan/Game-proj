4accdf76cdca342b706b20ffb40f61a025c51c51
georgebirgoan <147092162+georgebirgoan@users.noreply.github.com>
2024-01-25 18:05:45 +0200

    Refactor Game Grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,32 +14,29 @@ const GameGrid = ({gameQuery}:Props) => {
     const {data,error,isLoading}=useGames(gameQuery);
     const skeletons=[1,2,3,4,5,6,7,8,9,10];
 
+  if (error) return <Text>{error}</Text>;
 
   return (
-  <>
-  
-  {error && <Text>{error}</Text>}
 
-  <SimpleGrid 
-  columns={{sm:1,md:2,lg:3,xl:4}} 
-  padding={'10px'}
-  spacing={6}
-  >
+    <SimpleGrid 
+    columns={{sm:1,md:2,lg:3,xl:4}} 
+    padding={'10px'}
+    spacing={6}
+    >
 
-  {isLoading && 
-  skeletons.map((skeleton) =>(
-    <GameCardConatiner key={skeleton}>
-      <GameCardSkeleton />
-  </GameCardConatiner>
-   ))}
+    {isLoading && 
+    skeletons.map((skeleton) =>(
+      <GameCardConatiner key={skeleton}>
+        <GameCardSkeleton />
+    </GameCardConatiner>
+    ))}
 
-  {data.map((game)=>
-  <GameCardConatiner key={game.id} >
-    <GameCard  game={game} ></GameCard>
-  </GameCardConatiner>
-   )}
-</SimpleGrid>
-  </>
-)}
+    {data.map((game)=>
+    <GameCardConatiner key={game.id} >
+      <GameCard  game={game} ></GameCard>
+    </GameCardConatiner>
+    )}
+  </SimpleGrid>
+  )}
 
-export default GameGrid
+  export default GameGrid
