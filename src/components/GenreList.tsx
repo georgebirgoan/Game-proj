4f4c9b1adce6d66afc7e58b4dd5563fa3a97cbeb
georgebirgoan <147092162+georgebirgoan@users.noreply.github.com>
2024-01-25 16:40:22 +0200

    Adding title for genres and fix images

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
+import { Button, HStack, Heading, Image, List, ListItem, Spinner } from "@chakra-ui/react";
 import useGenre, { Genre } from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -14,18 +14,27 @@ const GenreList = ({onSelecetGenre,selectedGenre}:Props) => {
     if(error) return null;
     if(isLoading) return <Spinner/>
     return (
+        <>
+        <Heading fontSize='2xl' marginBottom='3' >Genres</Heading>
         <List>
             {data.map(genre=>
             <ListItem key={genre.id} paddingY={'5px'}>
                 <HStack>
-                    <Image boxSize={'32px'} borderRadius={8}
-                    src={getCroppedImageUrl(genre.image_background)} />
-                    <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal' } onClick={()=>onSelecetGenre(genre)} 
+                    <Image 
+                        boxSize={'32px'} 
+                        borderRadius={8}
+                        objectFit='cover'
+                        src={getCroppedImageUrl(genre.image_background)} />
+
+                    <Button whiteSpace='normal' 
+                        textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+                        onClick={()=>onSelecetGenre(genre)} 
                         variant='link' fontSize='lg'>{genre.name}</Button>
                 </HStack>
 
             </ListItem>)}
         </List>
+        </>
   )
 }
 
