f480cdc82208dc824b484ee90bae34d105e36a40
georgebirgoan <147092162+georgebirgoan@users.noreply.github.com>
2024-01-25 18:02:50 +0200

    Cuostomize the theme to get darker grays

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -7,6 +7,7 @@ export interface Genre{
     image_background:string;
 }
 //useData<Genre>('/genres'); -->use static data,no call data in server
+//we depends from another server,this is break down usualy
 const useGenre=()=>({data:genres,isLoading:false,error:null})
 
 
